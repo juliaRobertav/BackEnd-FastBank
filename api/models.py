5f25f389489da1d902125c65d4c33ac78eaec6d1
juliaRobertav <juguirald@gmail.com>
2023-11-27 15:51:13 -0300

    commit models jrbank

--- api/models.py
+++ api/models.py
@@ -4,15 +4,55 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
+#TESTAR: 
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    complemento = models.CharField(max_length=50, blank=True, null=True)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    cep = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.rua}, {self.numero} - {self.cidade}/{self.estado} - CEP: {self.cep}'
+    
+    
+# class Cadastro(models.Model):
+    
+#     def upload_imagem_cliente(instance, filename):
+#         return f"{instance.conta}-{filename}"
+    
+#     nome = models.CharField(max_length=50)
+#     nasc = models.CharField(max_length=50)
+#     cpf = models.IntegerField()
+#     email = models.CharField(max_length=50)
+#     senha = models.CharField(max_length=50)
+    
+#     conta = models.CharField(max_length=10, unique=True, default='00000000')
+#     limite = models.FloatField(null=True)
+#     # criado_em = models.DateTimeField(auto_now_add=True)
+#     agencia = models.CharField(max_length=10,  default='0000')
+#     # ultima_movimentacao = models.DateTimeField(auto_now_add=True)
+#     saldo = models.DecimalField(decimal_places=2, max_digits=30, default=0.00)
+#     telefone = models.CharField(max_length=15, default=000.000000)
+#     imagem = models.ImageField(upload_to=upload_imagem_cliente, blank=True, null=True)
+#     endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE, null=True)  
+    
+#     def __str__(self):
+#         return f'{self.nome} - {self.endereco} - {self.telefone} - R$ {self.saldo}'
+
 class Cadastro(models.Model):
     nome = models.CharField(max_length=50)
     nasc = models.CharField(max_length=50)
     cpf = models.IntegerField()
+    telefone = models.CharField(max_length=15, default='00.00000.0000')
+    endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE, null=True)  
     email = models.CharField(max_length=50)
     senha = models.CharField(max_length=50)
     
     def __str__(self):
-        return f'{self.nome} - {self.cliente} - R$ {self.saldo}'
+        return f'{self.nome} - {self.telefone} - R$ {self.saldo}'
     
     
 
@@ -35,8 +75,7 @@ class Cliente(models.Model):
     
 class Transacao(models.Model):
 
-    conta_cliente = models.ForeignKey(Cliente, null=True, verbose_name='Cliente', related_name='transacoes',
-                                       on_delete=models.PROTECT)
+    conta_cliente = models.ForeignKey(Cliente, null=True, verbose_name='Cliente', related_name='transacoes',on_delete=models.PROTECT)
     descricao = models.CharField(max_length=120, null=False)
     valor = models.FloatField(null=False)
     data_hora = models.DateTimeField(auto_now_add=True)
@@ -47,7 +86,7 @@ class Transacao(models.Model):
 
 class Contas(models.Model):
     agencia = models.CharField(max_length=10)
-    cliente = models.OneToOneField('Cliente', on_delete=models.CASCADE)
+    # cliente = models.OneToOneField('Cliente', on_delete=models.CASCADE)
     conta = models.CharField(max_length=10)
     saldo = models.DecimalField(decimal_places=2, max_digits=30, default=0.00)
     ultima_movimentacao = models.DateTimeField(auto_now=True)
